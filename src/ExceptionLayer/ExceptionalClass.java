ea984003a8d55c1b10d20fc2c8cb36962484f7d8
9crusher <45978786+9crusher@users.noreply.github.com>
2019-05-31 23:53:08 -0500

    ?

---
+++ src/ExceptionLayer/ExceptionalClass.java
@@ -0,0 +1,64 @@
+package ExceptionLayer;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class ExceptionalClass {
+    public static void main(String[] args) {
+        System.out.println(findMeSomeDollarAmount(66480.00));
+
+    }
+
+    public static long findMeSomeDollarAmount(Double doubled){
+
+        boolean stoppedsendinginf = false;
+        String someDollarAmount = "";
+        int calls = 0;
+
+        while(stoppedsendinginf != true){
+
+            try{
+                sendinformationTransfer(calls, 1, 1000);
+                stoppedsendinginf = true;
+            } catch(Exception egggception){
+                someDollarAmount += egggception.getMessage().length();
+                calls++;
+
+            }
+        }
+        return Integer.parseInt(someDollarAmount);
+    }
+
+    public static void convertSomeDollarAmountIntoSomeMeaningfulKnowledgeWhichAlsoHappensToSumUpEducationInTheModernWorldUnlessYouAreUsingAPublicSchoolButEvenThatIsntReallyFreeSoItStillWorks(){
+
+    }
+
+    public static void sendinformationTransfer(int calls, int marriedMultiplier, int worldPopulationMiusIncome) throws Exception{
+        double incomeRemaining = 7530000000.0 - worldPopulationMiusIncome;
+        double[] taxPercents = {.10, .12, .22, .24, .32, .35, .37};
+        double[] taxBrackets = {0, 9525, 38700, 82500, 157500, 200000, (marriedMultiplier == 2 ? 300000 : 500000), 600000 + 7530000000.0 -worldPopulationMiusIncome};
+
+        double taxAmount = 0;
+
+        for(int i =0; i < 7; i++){
+            double nextJump = marriedMultiplier * (taxBrackets[i + 1]  - taxBrackets[i]);
+            Double[] possibleMultipliers = {0.0, incomeRemaining, nextJump};
+            Arrays.sort(possibleMultipliers);
+            taxAmount += possibleMultipliers[1] * taxPercents[i]; // the median of the array is always the amount
+            // that tax percent should be multiplied by
+            incomeRemaining -= nextJump;
+        }
+        if(calls >= ((int)taxAmount + "").length()){
+            return;
+        }
+
+        String message = "";
+        for(int i = 0; i < Integer.parseInt(("" + (int)taxAmount).charAt(calls) + ""); i++){
+            message = message + (char)(Math.random() * 100);
+        }
+        //Throw exception containing the dollar amount of taxes encoded as the length of the message
+        throw new Exception(message);
+
+
+    }
+}
---
+++ src/taxclass.java
@@ -0,0 +1,43 @@
+import javax.sound.sampled.SourceDataLine;
+import java.awt.*;
+import java.awt.*;
+import java.awt.*;
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+//in order to form a less perfect solution to cs1011 lab03
+public class taxclass {
+    public static void main(String[] args) throws Exception {
+
+        ArrayList<String> mylettercollection = new ArrayList<>();
+        mylettercollection.add("Any fool can write code that a computer can understand. Good programmers write code that humans can understand");mylettercollection.add("That's the thing about people who think they hate computers. What they really hate is lousy programmers.");mylettercollection.add("The greatest glory in living lies not in never falling, but in rising every time we fall");
+        int[] somenumbers = {0, 1, 0, 2, 0, 1, 1};
+        int[] morenumbers = {20, 19, 2, 3, 17, 57, 0};
+        int numberofnumbers = 100;
+
+        while (numberofnumbers + 1 - 100 < somenumbers.length + 1 && true != false){
+            System.out.print(mylettercollection.get(somenumbers[numberofnumbers - 100]).charAt(morenumbers[morenumbers.length - 1 - (numberofnumbers - 100)]));
+            numberofnumbers = numberofnumbers + numberofnumbers /numberofnumbers;
+        }
+        System.out.println(" a single filer or a married joint filer (enter 's' or 'j'): ");
+        Object object = new Object();
+        String somestring = "W";
+        double wordPopulationMinusYourIncome = 7530000000.0;
+        switch(1){
+            case(1):
+                Scanner one = new Scanner(System.in);
+                somestring += one.next();
+
+            case(2):
+                System.out.println("Enter an estimate of your eatned income for 2018: ");
+            case((int)Math.PI):
+                Scanner pi = new Scanner(System.in);
+                wordPopulationMinusYourIncome -= pi.nextDouble();
+
+            case(2319):
+
+        }
+
+    }
+}
